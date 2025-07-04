e910fb02040097b8887389f798f5500b26c33ad8
Penny <cloverz1128@gmail.com>
2025-07-05 00:00:59 +1000

    Login response from django twitter-api

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,15 +1,42 @@
 import {
   Button, Input, Form, Dialog,
 } from 'antd-mobile';
+import { loginService } from '../../services/login';
 import './index.css';
 
 const Login = () => {
   const [form] = Form.useForm();
-  const onSubmit = () => {
+
+  const onSubmit = async () => {
     const values = form.getFieldsValue();
-    Dialog.alert({
-      content: <pre>{JSON.stringify(values, null, 2)}</pre>,
-    });
+
+    try {
+      const res = await loginService(values.username, values.password);
+      if (res.success) {
+        Dialog.alert({
+          content: <pre>{JSON.stringify(res.user, null, 2)}</pre>,
+        });
+      } else {
+        Dialog.alert({
+          content: res.message || 'An unknown success error occurred.',
+        });
+      }
+    } catch (err) {
+      if (err.response) {
+        const data = err.response.data;
+        const errorMessage = data.message || 'Login failed due to an unknown error.';
+
+        Dialog.alert({
+          content: errorMessage
+        });
+
+      } else {
+        Dialog.alert({
+          content: 'Network error. Please check your connection.'
+        });
+      }
+    }
+
   };
 
   return (
