5fed3a9918636b1313cc40dd7b2e44cf74b19325
Penny <cloverz1128@gmail.com>
2025-07-07 19:38:45 +1000

    implement datepickerInput component

--- src/components/DatePickerInput/index.js
+++ src/components/DatePickerInput/index.js
@@ -1,12 +1,45 @@
 import datepickerIcon from '@assets/datepicker-icon.svg';
+import { DatePicker } from 'antd-mobile';
+import moment from 'moment';
+import PropTypes from 'prop-types';
 import style from './index.module.css';
+import { useState } from 'react';
 
-export default () => (
-  <div className={style.birthdayInput}>
-    <div className={style.birthdayTitleItem}>Data of Birth</div>
-    <div>
-      <spam className={style.birthdayPlaceholder}>Year/Month/Day</spam>
-      <img className={style.datepickerIcon} src={datepickerIcon} alt='datepickerIcon' />
-    </div>
-  </div>
-);
+const DatePickerInput = ({
+  value,
+  onChange
+}) => {
+  const [visible, setVisible] = useState(false);
+  const onClickDatePicker = () => {
+    setVisible(true);
+  };
+  return (
+    <>
+      <DatePicker
+        title="Date Selector"
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        onConfirm={(val) => {
+          console.log(val)
+          onChange(val);
+        }}
+      />
+      <div className={style.birthdayInput} onClick={onClickDatePicker}>
+        <div className={style.birthdayTitleItem}>Data of Birth</div>
+        <div>
+          <span className={style.birthdayPlaceholder}> { value ? moment(value).format('YYYY/MM/DD') : 'Year/Month/Day'}</span>
+          <img className={style.datepickerIcon} src={datepickerIcon} alt='datepickerIcon' />
+        </div>
+      </div>
+    </>
+  );
+};
+
+DatePickerInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default DatePickerInput;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import Register from './containers/Register';
 import { startVconsole } from './utils';
 
+import { ConfigProvider } from 'antd-mobile';
+import enUS from 'antd-mobile/es/locales/en-US';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Register />
+    <ConfigProvider locale={enUS}>
+      <Register />
+    </ConfigProvider>
   </React.StrictMode>,
 );
 
